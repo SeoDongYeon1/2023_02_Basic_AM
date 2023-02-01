210c08b10c5b94c94a80c9ac260ace1c6246d696
SeoDongYeon1 <ehddus2655@gmail.com>
2023-02-01 15:51:03 +0900

    명령어 추가중(article write 추가중)

--- src/com/KoreaIT/java/BasicAM/Main.java
+++ src/com/KoreaIT/java/BasicAM/Main.java
@@ -9,14 +9,38 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 
+		int count = 0;
 		while (true) {
 			System.out.printf("명령어를 입력하세요 : ");
 			String command = sc.nextLine();
-			System.out.printf("입력된 명령어 : %s\n", command);
 
-			if (command.equals("system exit")) {
+			if (command.equals("article write")) {
+				count++;
+				System.out.printf("제목 :");
+				String command1 = sc.nextLine();
+				System.out.printf("내용 :");
+				String command2 = sc.nextLine();
+				System.out.printf("%d번글이 생성되었습니다.\n", count);
+				
+			} 
+			else if (command.equals("article list")) 
+			{
+				if(count!=0)
+				{
+					System.out.printf("%d개의 게시글이 존재합니다.\n", count);
+				}
+				else {
+					System.out.println("게시글이 없습니다.");	
+				}
+			} 
+			else if (command.equals("system exit")) 
+			{
 				break;
+			} 
+			else {
+				System.out.println("존재하지 않는 명령어입니다.");
 			}
+
 		}
 
 		System.out.println("==프로그램 끝==");
